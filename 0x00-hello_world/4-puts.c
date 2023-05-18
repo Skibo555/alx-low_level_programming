eedc359123c2168cd68492cf4cace944f112b07a
Julius Yonwatode <juliusyonwatade@gmail.com>
2023-05-18 12:41:54 -0700

    I came prepared ALX'

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main = Entry ponit.
+ *
+ * Return: 0 Always Success.
+ *
+ */
+int main()
+{
+	char assignment[] = "\"Programming is like building a multilingual puzzle";
+	puts(assignment);
+
+	return 0;
+}
