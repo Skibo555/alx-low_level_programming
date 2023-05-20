089ea5d15260e9c23d554419328e24699b5ea6a8
Julius Yonwatode <juliusyonwatade@gmail.com>
2023-05-19 19:08:42 -0700

    Alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Return: 0 Always success.
+ *
+ */
+int main(void)
+{
+	int alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+		putchar('\n');
+	}
+
+
+	return (0);
+}
