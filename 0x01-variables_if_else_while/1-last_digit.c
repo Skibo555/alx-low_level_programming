a673df9d7a3f0f3ad140e7f553d9422887d9b5ee
Julius Yonwatode <juliusyonwatade@gmail.com>
2023-05-19 18:48:07 -0700

    God have mercy

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,28 +5,29 @@
 /**
  * main - Entry Point.
  *
- * Return: 0 Always sSuccess.
+ * Return: 0 Always Success.
  *
  */
 
 int main(void)
 {
-	int n = 0;
 	int last;
-
-	last = n % 10;
+	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 5)
+
+	last = n % 10;
+
+	if (last > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, last);
 	}
-	if (n == 0)
+	if (last == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, last);
 	}
-	if (n < 6 && n != 0)
+	if (last < 6 && n != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 	}
