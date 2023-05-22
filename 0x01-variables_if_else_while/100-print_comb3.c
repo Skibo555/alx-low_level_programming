2b31f86a6b3a73e3be21ca28ef6907ab2d2a6960
Julius Yonwatode <juliusyonwatade@gmail.com>
2023-05-22 14:50:44 -0700

    There won't be an excuse for me to quit C this time

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Return: 0 Always.
+ *
+ */
+int main(void)
+{
+	int numbers;
+	int number;
+
+	for (numbers = '0'; numbers <= '8'; numbers++)
+	{
+		for (number = '1'; number <= '9'; number++)
+		{
+			if (numbers != number && number != numbers)
+				putchar(numbers);
+				putchar(number);
+				if (numbers != '8' || number != '9')
+				putchar(',');
+				putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
