32ad7f1d3b2ab5d95df78e6e9c4535d5446196ba
Julius Yonwatode <juliusyonwatade@gmail.com>
2023-05-23 22:15:57 -0700

    I love C

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,6 +3,8 @@
 /**
  * _islower - Checks if the character is lower or capital letter.
  *
+ * @c: is a paramerter that takes agument of type int.
+ *
  * Return: 1 if the character is lowercase and 0 if  otherwise.
  *
  */
@@ -10,7 +12,7 @@
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
-	       return (1);
+		return (1);
 	else
 		return (0);
-}	
+}
