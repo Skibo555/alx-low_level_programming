e34731a3ae6bec3306b6c16d6e0d5e2e83098270
Julius Yonwatode <juliusyonwatade@gmail.com>
2023-05-24 06:26:53 -0700

    absolute zero

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - converts in put to its absolute value.
+ *
+ * @n: the argument.
+ *
+ * Return: answer.
+ *
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n * 1);
+	else
+		return (n);
+}
