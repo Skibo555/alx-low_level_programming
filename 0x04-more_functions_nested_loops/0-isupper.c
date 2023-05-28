cd3943a5ec7babe7f1fba282a3816941e5aca4f8
Julius Yonwatode <juliusyonwatade@gmail.com>
2023-05-28 14:25:47 -0700

    C sucks as fuck'

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,7 +3,7 @@
 /**
  * _isupper - it checks if the input is an upper case character of an alphabet.
  *
- * @a: the inpu.
+ * @c: the input.
  *
  * Return: a success.
  *
@@ -11,7 +11,7 @@
 
 int _isupper(int c)
 {
-	if ( c >= 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
