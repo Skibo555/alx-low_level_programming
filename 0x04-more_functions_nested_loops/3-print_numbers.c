02d395851b7835bf9f1a8034f62f74832f9d1141
Julius Yonwatode <juliusyonwatade@gmail.com>
2023-05-28 15:16:54 -0700

    I love the brushing

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - it print numbers with _putchar.
+ *
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+
+		_putchar(n);
+
+	_putchar('\n');
+}
