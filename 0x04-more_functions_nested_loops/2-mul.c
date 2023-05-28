8d9ec71877ffab7f9453191e5af1795441facfd9
Julius Yonwatode <juliusyonwatade@gmail.com>
2023-05-28 14:54:30 -0700

    I love C'

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - it multiplies two int.
+ *
+ * @a: input first.
+ * @b: input second.
+ *
+ * Return: result.
+ */
+
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
