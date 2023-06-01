c477623177136f9932c69977492fe37e4f61aab7
Julius Yonwatode <juliusyonwatade@gmail.com>
2023-05-31 23:45:31 -0700

    I am late for work.

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_rev - it prints string in reverse.
+ *
+ * @s: the string to be printed.
+ *
+ */
+
+void print_rev(char *s)
+{
+	int i, len;
+
+	len = strlen(s);
+	i = len - 1;
+	while (len >= i)
+	{
+		_putchar(s[i]);
+		i--;
+		if (i < 0)
+			break;
+	}
+	_putchar('\n');
+}
